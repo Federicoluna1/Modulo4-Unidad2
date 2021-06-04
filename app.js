b73e403379731533051d13e59c5c09876d9255cd
Fede <84208153+Federicoluna1@users.noreply.github.com>
2021-06-04 17:14:43 -0300

    Updated

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const unless = require('express-unless');
+
+const app = express();
+const port = process.env.PORT ? process.env.PORT : 3000;
+
+app.use(express.static('${__dirname}/public'));
+
+app.use(express.json());
+
+const equiposRuta = require('./routes/equiposRuta');
+const jugadoresRuta = require('./routes/jugadoresRuta');
+const webRuta = require('./routes/webRuta');
+
+app.use('/api/equipos', equiposRuta);
+app.use('/api/jugadores', jugadoresRuta);
+app.use('/', webRuta);
+
+app.get('/', (req, res)=>{
+    res.json({mensaje: "hola"})
+ })
+
+app.use((req, res, next)=>{
+    const error = new Error('No se encontro la pagina');
+    error.status = 404;
+    next(error)
+})
+
+app.use((err, req, res, next)=>{
+    if(err){
+    console.log(err)
+    res.json({Error: err.message});
+    }
+})
+
+app.listen (port, ()=>{
+    console.log ('Servidor escuchando en el puerto' + port)
+})
+
